7cafaf50fab7881f735ec95ac6f6b28bd47c79a1
carlos <c.delriofrances@gmail.com>
2024-11-12 11:47:14 +0100

    added image functionality

--- app/new.tsx
+++ app/new.tsx
@@ -1,19 +1,28 @@
 import { PlantlyButton } from '@/components/PlantlyButton'
 import { PlantlyImage } from '@/components/PlantlyImage'
+import { usePlantStore } from '@/store/plantsStore'
 import { theme } from '@/theme'
+import * as ImagePicker from 'expo-image-picker'
+import { useRouter } from 'expo-router'
 import { useState } from 'react'
 import {
   Alert,
-  ScrollView,
+  Platform,
   StyleSheet,
   Text,
   TextInput,
+  TouchableOpacity,
   View
 } from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 export default function NewScreen() {
+  const router = useRouter()
+  const addPlant = usePlantStore((state) => state.addPlant)
+
   const [name, setName] = useState<string>()
   const [days, setDays] = useState<string>()
+  const [selectedImage, setSelectedImage] = useState<string>()
 
   const handleSubmit = () => {
     if (!name) {
@@ -34,18 +43,76 @@ export default function NewScreen() {
       )
     }
 
-    console.log('Adding plant', name, days)
+    addPlant(name, Number(days))
+    router.navigate('/')
+  }
+
+  const handleChooseImage = async () => {
+    if (Platform.OS === 'web') {
+      return
+    }
+
+    Alert.alert(
+      'Choose Image Source',
+      'Do you want to pick an image from the library or take a photo?',
+      [
+        {
+          text: 'Library',
+          onPress: () => pickImageFromLibrary()
+        },
+        {
+          text: 'Camera',
+          onPress: () => takePhoto()
+        },
+        {
+          text: 'Cancel',
+          style: 'cancel'
+        }
+      ]
+    )
+  }
+
+  const pickImageFromLibrary = async () => {
+    await ImagePicker.requestMediaLibraryPermissionsAsync()
+    const imageResult = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1
+    })
+    console.log(JSON.stringify(imageResult, null, 2))
+    if (!imageResult.canceled) {
+      setSelectedImage(imageResult.assets[0].uri)
+    }
+  }
+
+  const takePhoto = async () => {
+    await ImagePicker.requestCameraPermissionsAsync()
+    const imageResult = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1
+    })
+    console.log(JSON.stringify(imageResult, null, 2))
+    if (!imageResult.canceled) {
+      setSelectedImage(imageResult.assets[0].uri)
+    }
   }
 
   return (
-    <ScrollView
+    <KeyboardAwareScrollView
       style={styles.container}
       contentContainerStyle={styles.contentContainer}
       keyboardShouldPersistTaps="handled"
     >
-      <View style={styles.centered}>
-        <PlantlyImage />
-      </View>
+      <TouchableOpacity
+        style={styles.centered}
+        activeOpacity={0.8}
+        onPress={handleChooseImage}
+      >
+        <PlantlyImage imageUri={selectedImage} />
+      </TouchableOpacity>
+
       <Text style={styles.label}>Name</Text>
       <TextInput
         value={name}
@@ -63,7 +130,7 @@ export default function NewScreen() {
         keyboardType="number-pad"
       />
       <PlantlyButton title="Add plant" onPress={handleSubmit} />
-    </ScrollView>
+    </KeyboardAwareScrollView>
   )
 }
 
--- theme.ts
+++ theme.ts
@@ -4,5 +4,7 @@ export const theme = {
   colorLeafyGreen: '#206a42',
   colorAppleGreen: '#a0d36c',
   colorLimeGreen: '#d0e57e',
-  colorLightGrey: '#eee'
+  colorLightGrey: '#eee',
+  colorGrey: '#808080',
+  colorBlack: '#000'
 }
