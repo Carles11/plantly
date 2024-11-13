7792bcf31535758042092ac8c9e765a8413cf7f5
carlos <c.delriofrances@gmail.com>
2024-11-13 11:59:03 +0100

    added leaf icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,21 @@
+import * as QuickActions from 'expo-quick-actions'
+import { useQuickActionRouting } from 'expo-quick-actions/router'
 import { SplashScreen, Stack } from 'expo-router'
-
+import { useEffect } from 'react'
+import { Platform } from 'react-native'
 // SplashScreen.preventAutoHideAsync()
 export default function Layout() {
+  useQuickActionRouting()
+  useEffect(() => {
+    QuickActions.setItems([
+      {
+        title: 'Add a plant',
+        icon: Platform.OS === 'ios' ? 'symbol:leaf' : 'leaf',
+        id: '0',
+        params: { href: '/new' }
+      }
+    ])
+  }, [])
   return (
     <Stack>
       <Stack.Screen
